a28d6fe9af18422561319e359460dea620628f47
Gabriel Tenorio Santos de Siqueira <gabriels.e@bauducco.com.br>
2024-09-04 12:23:27 -0300

    adicionado função de busca

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-alert('Oi');
+function pesquisar() {
+    // Obtém o elemento de input onde o usuário digita o nome do atleta
+    let inputPesquisar = document.getElementById('input-pesquisar');
+  
+    // Obtém o elemento onde o resultado da pesquisa será exibido
+    let resultadoPesquisa = document.getElementById('resultado-pesquisa'); 
+  
+    // Busca o índice do objeto atleta no array de dados que corresponda ao valor digitado
+    const indiceDoObjetoAtleta = dados.findIndex(nome => nome.titulo === inputPesquisar.value);
+  
+    // Verifica se o atleta foi encontrado (índice diferente de -1)
+    if (indiceDoObjetoAtleta !== -1) {
+      // Remove o atributo 'hidden' para exibir o resultado
+      resultadoPesquisa.removeAttribute('hidden');
+  
+      // Preenche o elemento com as informações do atleta encontrado
+      resultadoPesquisa.innerHTML = `
+        <h2><a href="#" target="_blank" id="titulo-atleta">${dados[indiceDoObjetoAtleta].titulo}</a></h2>
+        <p class="descricao-meta" id="descricao-atleta">${dados[indiceDoObjetoAtleta].descricao}</p>
+        <a href="${dados[indiceDoObjetoAtleta].link}" target="_blank" id="sobre-atleta">Mais informações</a>
+      `;
+    } else {
+      // Se o atleta não foi encontrado, exibe uma mensagem
+      resultadoPesquisa.removeAttribute('hidden');
+      resultadoPesquisa.textContent = `Atleta não encontrado.`
+    }
+  }
---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, conhecida como 'Fadinha', é uma skatista brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e carisma, ela conquistou fãs ao redor do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024, tornando-se um dos maiores nomes do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas em competições internacionais, incluindo os Jogos Olímpicos. Conhecida por sua força e elegância, ela é um dos maiores talentos da ginástica artística brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    }
+];
